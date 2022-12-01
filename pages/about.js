5653a188ac9677889db00b72850368bc36c1fd91
yasuhironagaya <69679135+yasuhironagaya@users.noreply.github.com>
2022-12-02 07:59:20 +0900

    ♻️ exporu defaultの修正

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { Footer } from "../components/Footer";
+import { Headline } from "../components/Headline";
 import { Links } from "../components/Links";
-import Headline from "./Headline";
+import { Footer } from "../components/Footer";
 
 export default function Home() {
   return (
