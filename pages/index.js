9e5c819ec343495e8034b95b64405b2972324603
yasuhironagaya <69679135+yasuhironagaya@users.noreply.github.com>
2022-12-02 21:00:49 +0900

    ♻️ メイン部分のコンポーネント化

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { Headline } from "../components/Headline";
-import { Links } from "../components/Links";
 import { Footer } from "../components/Footer";
+import { Main } from "../components/Main";
 
 export default function Home() {
   return (
@@ -13,10 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <Headline page="index" />
-        <Links />
-      </main>
+      <Main page="index" />
 
       <Footer />
     </div>
