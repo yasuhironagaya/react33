7cbc5e3732d3b404a93416a263d7041fd686b98b
yasuhironagaya <69679135+yasuhironagaya@users.noreply.github.com>
2022-12-09 22:12:31 +0900

    ⚡️ useCallback,useEffect

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,19 +2,22 @@ import classes from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(1);
 
-  const handleClick = (e) => {
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-  };
+  const handleClick = useCallback(
+    () => {
+      if (count < 10) {
+        setCount((count) => count + 1);
+      }
+    },
+    [count]
+  );
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
-
     return () => {
       document.body.style.backgroundColor = "";
     };
