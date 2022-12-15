f204dad4ec89c2163429e8f8c6452bad777fc1d4
yasuhironagaya <69679135+yasuhironagaya@users.noreply.github.com>
2022-12-15 22:03:09 +0900

    ✨ useStateで配列

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import classes from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
@@ -8,8 +9,7 @@ export default function Home() {
   const [count, setCount] = useState(1);
   const [text, setText] = useState("");
   const [isShow, setIsShow] = useState(true);
-
-  console.log({ text });
+  const [array, setArray] = useState([]);
 
   const handleClick = useCallback(() => {
     if (count < 10) {
@@ -24,8 +24,18 @@ export default function Home() {
     setText(e.target.value.trim());
   }, []);
 
+  const handleAdd = useCallback(() => {
+    setArray((prevArray) => {
+      if (prevArray.some((item) => item === text)) {
+        alert("すでに存在する値です");
+        return prevArray;
+      }
+        return [...prevArray, text];
+    });
+  }, [text]);
+
   const handleDisplay = useCallback(() => {
-    setIsShow((isShow) => !isShow);
+    setIsShow((prevIsShow) => !prevIsShow);
   }, []);
 
   useEffect(() => {
@@ -35,7 +45,6 @@ export default function Home() {
     };
   }, []);
 
-
   return (
     <div className={classes.container}>
       <Header />
@@ -43,6 +52,12 @@ export default function Home() {
       <button onClick={handleClick}>ボタン</button>
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
       <input type="text" value={text} onChange={handleChange} />
+      <button onClick={handleAdd}>追加</button>
+      <ul>
+        {array.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+      </ul>
 
       <Main page="index" />
       <Footer />
