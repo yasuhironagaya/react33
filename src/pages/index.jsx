c07d2bf7c89d3e3e98098bfaea1f42c562b7829a
yasuhironagaya <69679135+yasuhironagaya@users.noreply.github.com>
2022-12-15 22:01:23 +0900

    ♻️ prevを追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,7 +13,7 @@ export default function Home() {
 
   const handleClick = useCallback(() => {
     if (count < 10) {
-      setCount((count) => count + 1);
+      setCount((prevCount) => prevCount + 1);
     }
   }, [count]);
   const handleChange = useCallback((e) => {
